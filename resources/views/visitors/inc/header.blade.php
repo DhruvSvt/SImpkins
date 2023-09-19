1448eab79291750ccf09aa8e4e7ba6150772995d
DhruvSvt <dhruvsvt08@gmail.com>
2023-09-19 18:18:46 +0530

    add some details in header

--- resources/views/visitors/inc/header.blade.php
+++ resources/views/visitors/inc/header.blade.php
@@ -21,8 +21,8 @@
         <div class="row">
             <div class="col-lg-2 col-12 text-center d-flex">
                 <a href="" class="m-auto">
-                    <img loading="lazy" src="{{ config('app.url') }}visitors/assets/img/logo/simpkins_logo.jpg" alt="logo"
-                    style="height: 94px;width: 106px;" aspect-ratio: 409/512">
+                    <img loading="lazy" src="{{ config('app.url') }}visitors/assets/img/logo/simpkins_logo.jpg"
+                        alt="logo" style="height: 94px;width: 106px;" aspect-ratio: 409/512">
                 </a>
             </div>
             <div class="col-lg-8 col-12">
@@ -37,6 +37,18 @@
                     </p>
                     <p class="address" style="text-transform: uppercase; font-size: 0.74rem">
                         Affiliated to central board of secondary education, New Delhi
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-2 col-12 text-center d-flex ">
+                <div class="m-auto">
+                    <?php
+                    date_default_timezone_set('Asia/Kolkata'); // Set the timezone to India (Asia/Kolkata)
+                    ?>
+                    <p class=".address{" style="font-size: 0.7rem;">
+                        CBSE Affiliation No. 1234567<br>
+                        Date : <?= date('Y-m-d') ?><br>
+                        Current Time: <?php echo date('H:i:s'); ?>
                     </p>
                 </div>
             </div>
