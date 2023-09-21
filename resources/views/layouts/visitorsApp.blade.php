7767294234f20ddd315955eb851eecf6079fe5e6
DhruvSvt <dhruvsvt08@gmail.com>
2023-09-21 17:09:18 +0530

    update footer

--- resources/views/layouts/visitorsApp.blade.php
+++ resources/views/layouts/visitorsApp.blade.php
@@ -32,6 +32,9 @@
     <link rel="shortcut icon" type="image/x-icon" href="{{ config('app.url') }}visitors/assets/img/features/icon.png">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap"
         rel="stylesheet">
+        <!-- Font Awsome cdn-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+
     <link rel="stylesheet" href="{{ config('app.url') }}visitors/assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{ config('app.url') }}visitors/assets/css/animate.min.css">
     <link rel="stylesheet" href="{{ config('app.url') }}visitors/assets/css/magnific-popup.css">
