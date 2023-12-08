329adbb6963f0d471a2d87412afed519a5460589
DhruvSvt <dhruvsvt08@gmail.com>
2023-12-08 14:27:27 +0530

    Update TimetableCollection.php

--- app/Http/Resources/TimetableCollection.php
+++ app/Http/Resources/TimetableCollection.php
@@ -15,14 +15,17 @@ class TimetableCollection extends ResourceCollection
     public function toArray($request) {
         $response = array();
         foreach ($this->collection as $key => $row) {
+            // if(!$row['subject_teacher'] || !$row['subject_teacher']['subject']){
+            //     array_splice($this->collection, $key, 1);
+            // }
             $response[$key] = array(
                 "start_time" => $row['start_time'],
                 "end_time" => $row['end_time'],
                 "day" => $row['day'],
                 "day_name" => $row['day_name'],
-                "subject" => $row['subject_teacher']['subject'],
-                "teacher_first_name" => $row['subject_teacher']['teacher']['user']['first_name'],
-                "teacher_last_name" => $row['subject_teacher']['teacher']['user']['last_name'],
+                "subject" => $row['subject_teacher'] ? $row['subject_teacher']['subject'] : (object)[],
+                "teacher_first_name" => $row['subject_teacher'] ? $row['subject_teacher']['teacher']['user']['first_name'] : '-',
+                "teacher_last_name" => $row['subject_teacher'] ? $row['subject_teacher']['teacher']['user']['last_name'] : '-',
             );
         }
         return $response;
