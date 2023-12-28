b5c0687aa0a389a8b61d0795a8f015e41ba90409
DhruvSvt <dhruvsvt08@gmail.com>
2023-12-28 11:08:51 +0530

    rearrange action button

--- resources/views/students/details.blade.php
+++ resources/views/students/details.blade.php
@@ -53,7 +53,12 @@
                                         <tr>
                                             <th scope="col" data-field="id" data-sortable="true" data-visible="false">
                                                 {{ __('id') }}</th>
+
                                             <th scope="col" data-field="no" data-sortable="true">{{ __('no') }}</th>
+                                            @canany(['student-edit', 'student-delete'])
+                                            <th data-events="studentEvents" scope="col" data-field="operate"
+                                                data-sortable="false">{{ __('action') }}</th>
+                                        @endcanany
                                             <th scope="col" data-field="user_id" data-sortable="false"
                                                 data-visible="false">{{ __('user_id') }}</th>
                                             <th scope="col" data-field="admission_no" data-sortable="false">
@@ -95,10 +100,7 @@
                                                 {{ __('is_only_child') }}</th>
                                             <th scope="col" data-field="is_minority_text" data-sortable="false">
                                                 {{ __('is_minority') }}</th>
-                                            @canany(['student-edit', 'student-delete'])
-                                                <th data-events="studentEvents" scope="col" data-field="operate"
-                                                    data-sortable="false">{{ __('action') }}</th>
-                                            @endcanany
+
                                         </tr>
                                     </thead>
                                 </table>
