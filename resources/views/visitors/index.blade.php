1a57a3810095a1e5d617e07d8a0aa8d90ca328ad
DhruvSvt <dhruvsvt08@gmail.com>
2024-01-08 17:12:30 +0530

    modal-dialog

--- resources/views/visitors/index.blade.php
+++ resources/views/visitors/index.blade.php
@@ -861,7 +861,8 @@
 
 <div id="imageModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="imageModalLabel" aria-hidden="true"
     style="padding-right:0px !important">
-    <div class="modal-dialog" style="    max-width: 800px;
+    <div class="modal-dialog" style="       max-width: 583px;
+    margin-top: 6px;
     ">
         <div class="modal-content">
             <div class="modal-body">
