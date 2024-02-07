56d28bb8d71dceed28231dded0344cbce3a9ceb4
DhruvSvt <dhruvsvt08@gmail.com>
2024-02-07 11:27:00 +0530

    Migrations done

---
+++ database/migrations/2024_02_03_120007_create_message_templates_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('message_templates', function (Blueprint $table) {
+            $table->id();
+            $table->string('template_name')->nullable();
+            $table->longText('template')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('message_templates');
+    }
+};
